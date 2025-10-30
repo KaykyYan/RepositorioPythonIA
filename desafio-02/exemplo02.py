41af3efc66eb5e05f041518cb42940a4863f539a
Kayky Yan <129062656+KaykyYan@users.noreply.github.com>
2025-10-29 22:31:52 -0300

    Ajuste, pois tinha feito o pull errado

--- desafio-02/exemplo02.py
+++ desafio-02/exemplo02.py
@@ -10,10 +10,6 @@ nome_produto = "Camiseta";
 preco_original = 50.00;
 porcentagem_desconto = 20;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> a51a0a3 (Realização dos exercicios da atividade 3)
 valor_desconto = preco_original * (porcentagem_desconto / 100);
 preco_final = preco_original - valor_desconto;
 
@@ -21,8 +17,4 @@ print(f"\nProduto: {nome_produto}")
 print(f"Preço original: R$ {preco_original:.2f}")
 print(f"Desconto: {porcentagem_desconto}%")
 print(f"Valor do desconto: R$ {valor_desconto:.2f}")
-<<<<<<< HEAD
 print(f"Preço final: R$ {preco_final:.2f}")
-=======
-print(f"Preço final: R$ {preco_final:.2f}")
->>>>>>> a51a0a3 (Realização dos exercicios da atividade 3)
